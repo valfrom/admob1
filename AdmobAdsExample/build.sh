808364b77ca62e91ff65d5a27e764a1224a0fe21
Valerii Ivanov <v.ivanov@keitaitoys.com>
2012-04-03 09:23:17 +0300

    Fixed file names in the adb parameters

--- AdmobAdsExample/build.sh
+++ AdmobAdsExample/build.sh
@@ -3,5 +3,5 @@
 mkb --buildenv=SCONS --debug
 mkb --buildenv=SCONS --debug --deploy=android
 
-#adb uninstall com.mycompany.s3eflurry
-#adb install build_s3eflurry_scons_x86/deployments/default/android/debug/s3eFlurry.apk
+adb uninstall com.mycompany.s3ehelloworld
+adb install build_s3ehelloworld_scons_x86/deployments/default/android/debug/s3ehelloworld.apk
